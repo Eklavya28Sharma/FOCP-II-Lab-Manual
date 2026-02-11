8c169a03be8171e1e7cf83d04340dbf602c7b1dd
Eklavya Sharma <eklavya28sharma@gmail.com>
2026-02-11 10:24:47 +0530

    Add files via upload

--- Q1.cpp
+++ Q1.cpp
@@ -1,23 +1,10 @@
 #include<iostream>
 using namespace std;
-
-bool isprime(int n){
-    int flag=0;
-    for(int i=2;i<n/2;i++){
-        if(n%i==0) {flag=1;}
-        else flag=0;
-        if(flag==0) return false;
-        else return true;
-    }
-}
 int main(){
-    int A;
-    cout<<"Enter number:";
-    cin>> A;
-
-    if(isprime(A)){
-         cout<<"Prime";
-    }
-    else cout<<"Not Prime";
+    int a,b,c;
+    cout<<"Enter the marks of students:";
+    cin>>a>>b>>c;
+    float avg= float(a+b+c)/3;
+    cout<<"the average marks are "<<float(avg);
     return 0;
 }
--- Q2.cpp
+++ Q2.cpp
@@ -1,26 +1,15 @@
+//An architect wants to calculate the space covered by a circular fountain. Implement a solution to compute 
+//the area of a circle. 
+
 #include<iostream>
+#include<cmath>
 using namespace std;
-void swapreference(int &a,int &b){ // It changes the a and b variables and it affects change x and y.
-    cout<<"In before swap: "<<a<<b<<endl;
-    int temp = a;
-    a=b;
-    b=temp;
-    cout<<"In after swap: "<<a<<b<<endl;
-}
-void swap(int a,int b){ // It only changes the a and b variables and does'nt change x and y.
-    cout<<"In before swap: "<<a<<b<<endl;
-    int temp = a;
-    a=b;
-    b=temp;
-    cout<<"In after swap: "<<a<<b<<endl;
-}
 int main(){
-    int x,y;
-    cin>>x>>y;
-    cout<<"in main: "<<x<<y<<endl;
-    swap(x,y);
-    cout<<"In main: "<<x<<y<<endl;
-    swapreference(x,y);
-    cout<<"In main: "<<x<<y;
-    return -1;
+    int radius;
+    double pi = M_PI;
+    cout<<"Enter the radius of the circle:";
+    cin>>radius;
+    float Area= M_PI*pow(radius,2);
+    cout<<"Area of fountain is:"<<Area;
+    return 0;
 }
